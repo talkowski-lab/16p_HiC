bf8df18a994ef0b4b70b11143ffe0c8ead46e4be
Siddharth Reed <slreed@andrew.cmu.edu>
2025-04-21 11:01:05 -0400

    Fix bugs, print more messages, only calculate for ideal smoothing params

--- scripts/run.hicrep.sh
+++ scripts/run.hicrep.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
 set -euo pipefail
-
-RESOLUTIONS=(500000 100000 50000 10000)
-HS=(1 3 5 20)
+# Fixed params
+RESOLUTIONS=(500000 100000 50000 25000 10000)
+get_smooting_param() {
+    # get relevant smoothing param for each resolution
+    # https://github.com/TaoYang-dev/hicrep
+    resolution=$1
+    case $resolution in 
+        1000000) h=0  ;;
+        500000)  h=1  ;;
+        100000)  h=3  ;;
+        50000)   h=4  ;;
+        40000)   h=5  ;;
+        25000)   h=10 ;;
+        10000)   h=20 ;;
+        5000)    h=40 ;;
+    esac
+    echo ${h}
+}
 MAX_WINDOW_SIZE=(100000 500000 1000000)
-DOWNSAMPLE=(0 1)
-
+DOWNSAMPLE=(1 0)
+# Unput files
 OUTPUT_DIR="${1}"
-[[ -d $OUTPUT_DIR ]] || (echo 'Invalid output dir' && exit 1)
+mkdir -p "${OUTPUT_DIR}"
 HIC_SAMPLES=${@:2}
 # All combos of hicrep params
+job_finished=0
+jobs_skipped=0
+for downsample in ${DOWNSAMPLE[@]}; do
 for max_window_size in ${MAX_WINDOW_SIZE[@]}; do
 for resolution in ${RESOLUTIONS[@]}; do
-for h in ${HS[@]}; do
-for downsample in ${DOWNSAMPLE[@]}; do
+    h="$(get_smooting_param ${resolution})"
     # For each possible valid combination of parameters
     if [[ $downsample -eq 0 ]]; then
-        downsample="" 
-        downsample_str="original" 
+        downsample_arg="" 
+        downsample_str="FALSE" 
     else
-        downsample="--bDownSample"
-        downsample_str="downsampled"
+        downsample_arg="--bDownSample "
+        downsample_str="TRUE"
     fi
     [[ $max_window_size -lt $resolution ]] && continue
     # For all combinations of samples
@@ -30,32 +47,32 @@ for downsample in ${DOWNSAMPLE[@]}; do
         sample_file_i="$(readlink -e ${sample_file_i})"
         sample_ID_i="$(basename $sample_file_i)"
         sample_ID_i="${sample_ID_i%%.mcool}"
-        sample_name_i="$(echo $sample_ID_i | cut -d'.' -f1)"
-        sample_params_i="$(echo $sample_ID_i | cut -d'.' -f2-)"
         # sample 2 info
         sample_file_j="$(readlink -e ${sample_file_j})"
         sample_ID_j="$(basename $sample_file_j)"
         sample_ID_j="${sample_ID_j%%.mcool}"
-        sample_name_j="$(echo $sample_ID_j | cut -d'.' -f1)"
-        sample_params_j="$(echo $sample_ID_j | cut -d'.' -f2-)"
         # Check if results are redundant or already exist
-        comparison_dir="${downsample_str}/${max_window_size}/${h}/${resolution}"
-        output_file="${OUTPUT_DIR}/${comparison_dir}/${sample_ID_i}.vs.${sample_ID_j}-hicrep.txt"
-        redundant_file="${OUTPUT_DIR}/${comparison_dir}/${sample_ID_j}.vs.${sample_ID_i}-hicrep.txt"
-        [[ -e ${output_file} || -e ${redundant_file} ]] && continue
+        comparison_dir="resolution_${resolution}/h_${h}/is.downsampled_${downsample_str}/window.size_${max_window_size}"
+        output_file="${OUTPUT_DIR}/${comparison_dir}/${sample_ID_i}-${sample_ID_j}-hicrep.txt"
+        redundant_file="${OUTPUT_DIR}/${comparison_dir}/${sample_ID_j}-${sample_ID_i}-hicrep.txt"
+        [[ -e ${output_file} || -e ${redundant_file} ]] && jobs_skipped=$(( $jobs_skipped+1 )) && continue 
+        [[ ${sample_ID_i} == ${sample_ID_j} ]] && continue
         # Run hicrep
         mkdir -p "${OUTPUT_DIR}/${comparison_dir}"
-        echo $output_file
-        hicrep $downsample \
-            --h $h \
-            --dBPMax $max_window_size \
-            --binSize $resolution \
-            $sample_file_i \
-            $sample_file_j \
-            $output_file
+        # echo $output_file
+        cmd="hicrep ${downsample_arg}--dBPMax ${max_window_size} --binSize ${resolution} --h ${h} ${sample_file_i} ${sample_file_j} ${output_file}"
+        echo ${cmd}
+        ${cmd}
+        # hicrep "${downsample_arg}"
+        #     --h "${h}" 
+        #     --dBPMax ${max_window_size} 
+        #     --binSize ${resolution} 
+        #     ${sample_file_i} 
+        #     ${sample_file_j}
+        #     ${output_file}
     done
     done
 done
 done
 done
-done
+echo "Finished ${job_finished} HiCRep runs and skipped re-calcuating ${jobs_skipped} runs"
