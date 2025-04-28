18b5049dcddfcfa1fc1ba6ee94cf53d2fe85aeba
Siddharth Reed <slreed@andrew.cmu.edu>
2025-04-28 11:20:54 -0400

    Update paths and results files

--- scripts/locations.R
+++ scripts/locations.R
@@ -5,19 +5,27 @@ BASE_DIR <- here()
 # Input Data
 GENOME_REF_DIR <-       '/data/talkowski/tools/ref/Hi_c_noalt'
 CHROMOSOME_SIZES_FILE <- file.path(GENOME_REF_DIR, 'hg38.reduced.chrom.sizes')
-FASTQ_DIR <-            file.path(BASE_DIR, 'fastq')
-SAMPLE_METADATA_FILE <- file.path(BASE_DIR, 'sample.metadata.tsv')
-# Results Dirs
-RESULTS_DIR <-          file.path(BASE_DIR, 'results')
-PAIRS_DIR <-            file.path(RESULTS_DIR, 'mapped_parsed_sorted_chunks')
-COOLERS_DIR <-          file.path(RESULTS_DIR, 'coolers_library')
-HICREP_DIR <-           file.path(RESULTS_DIR, 'hicrep')
-HICREP_RESULTS_FILE <-  file.path(HICREP_DIR, 'all.hicrep.scores.tsv')
-MATRIX_QC_DIR <-        file.path(RESULTS_DIR, 'sample.QC')
-ELISE_RECREATION_DIR <- file.path(RESULTS_DIR, 'Elise.Recreation.results')
-TAD_DIR <-              file.path(RESULTS_DIR, 'TADs')
-# SPARSE_MATRIX_DIR <-    file.path(RESULTS_DIR, 'sparse.matrices')
-# MULTIHICCOMPARE_DIR <-  file.path(RESULTS_DIR, 'multiHiCCompare')
+FASTQ_DIR <-             file.path(BASE_DIR, 'fastq')
+SAMPLE_METADATA_FILE <-  file.path(BASE_DIR, 'sample.metadata.tsv')
+# distiller produced ou  puts
+RESULTS_DIR <-           file.path(BASE_DIR, 'results')
+BAM_DIR <-               file.path(RESULTS_DIR, 'mapped_parsed_sorted_chunks')
+PAIRS_DIR <-             file.path(RESULTS_DIR, 'pairs_library')
+COOLERS_DIR <-           file.path(RESULTS_DIR, 'coolers_library')
+# Used for Sample QCing  
+SAMPLE_QC_DIR <-         file.path(RESULTS_DIR, 'sample.QC')
+COVERAGE_DIR <-          file.path(SAMPLE_QC_DIR, 'coverage')
+COVERAGE_DATA_FILE <-    file.path(COVERAGE_DIR, 'all.coverage.data.tsv')
+# hicrep results
+HICREP_DIR <-            file.path(RESULTS_DIR, 'hicrep')
+HICREP_RESULTS_FILE <-   file.path(HICREP_DIR, 'all.hicrep.scores.tsv')
+# Reproducing figure fr  m Elise Robinson
+ELISE_RECREATION_DIR <-  file.path(RESULTS_DIR, 'Elise.Recreation.results')
+# Differential Contact   esults from multiHiCCompare
+MULTIHICCOMPARE_DIR <-   file.path(RESULTS_DIR, 'multiHiCCompare')
+SPARSE_MATRIX_DIR <-     file.path(MULTIHICCOMPARE_DIR, 'sparse.matrices')
+# TAD Annotations
+TAD_DIR <-               file.path(RESULTS_DIR, 'TADs')
 # Factor levels for variaous metadata categories
 EDITS <- 
     c(
