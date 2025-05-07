f64ff84d6d44b051d839a9c1741e0d77844e88f6
Siddharth Reed <slreed@andrew.cmu.edu>
2025-05-07 08:25:32 -0400

    update dependencies

--- scripts/utils.TADs.R
+++ scripts/utils.TADs.R
@@ -1,8 +1,12 @@
-# Dependencies 
-# library(tidyverse)
-# library(magrittr)
-# library(glue)
-# library(purrr)
+library(tidyverse)
+# library(HiContacts)
+library(glue)
+library(furrr)
+library(ggplot2)
+library(viridis)
+library(ggh4x)
+# library(ggtext)
+library(cowplot)
 ###############
 # Load TAD annotation data
 load_arrowhead_TAD_annotation <- function(
@@ -84,6 +88,8 @@ load_TAD_annotation <- function(
         load_hiTAD_TAD_annotation(filepath)
     } else if (method == 'hiTAD-DIs') {
         load_hiTAD_DIs(filepath)
+    } else if (method == 'cooltools') {
+        load_cooltools_TAD_annotation(filepath)
     } else if (method == 'arrowhead') {
         load_arrowhead_TAD_annotation(filepath)
     } else {
@@ -91,9 +97,7 @@ load_TAD_annotation <- function(
     }
 }
 
-load_all_TAD_annotations <- function(
-    tad.method,
-    ...){
+load_all_TAD_annotations <- function(){
     parse_results_filelist(
         input_dir=TAD_DIR,
         suffix='-TAD.tsv',
@@ -101,19 +105,18 @@ load_all_TAD_annotations <- function(
         param_delim='_',
     ) %>%
     process_matrix_name() %>% 
+    group_by(method) %>% slice_head(n=2) %>% ungroup() %>% 
     mutate(
         TADs=
             pmap(
                 .,
                 load_TAD_annotation,
-                method=tad.method,
                 .progress=TRUE
             )
     ) %>%
     select(-c(filepath)) %>%
     unnest(TADs)
 }
-
 ###############
 # Compute stuff
 
