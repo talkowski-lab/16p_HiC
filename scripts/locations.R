a17ac1f96f9dee9756f76401e83618727ac33334
DJSiddharthVader <sidreed34@gmail.com>
2025-06-27 00:31:21 -0400

    update paths

--- scripts/locations.R
+++ scripts/locations.R
@@ -2,9 +2,9 @@ library(here)
 # BASE_DIR <- here()
 # Input Data
 GENOME_REF_DIR                 <- '/data/talkowski/tools/ref/Hi_c_noalt'
-CHROMOSOME_SIZES_FILE          <- file.path(GENOME_REF_DIR, 'hg38.reduced.chrom.sizes')
+CHROMOSOME_SIZES_FILE          <- file.path(GENOME_REF_DIR, 'GRCh38_no_alt_analysis_set_GCA_000001405.15.chrom.sizes')
 FASTQ_DIR                      <- file.path(BASE_DIR, 'fastq')
-SAMPLE_METADATA_FILE           <- file.path(BASE_DIR, 'sample.metadata.tsv')
+SAMPLE_METADATA_FILE           <- file.path(BASE_DIR, '16p.sample.metadata.tsv')
 # distiller produced ou           puts
 RESULTS_DIR                    <- file.path(BASE_DIR, 'results')
 BAM_DIR                        <- file.path(RESULTS_DIR, 'mapped_parsed_sorted_chunks')
@@ -13,6 +13,8 @@ COOLERS_DIR                    <- file.path(RESULTS_DIR, 'coolers_library')
 # Used for Sample QCing           
 SAMPLE_QC_DIR                  <- file.path(RESULTS_DIR, 'sample.QC')
 COVERAGE_DIR                   <- file.path(SAMPLE_QC_DIR, 'coverage')
+RESOLTION_COVERAGE_SUMAMRY_FILE <- file.path(SAMPLE_QC_DIR, 'resolution.coverage.summaries.tsv')
+MIN_SAMPLE_RESOLUTION_FILE     <- file.path(SAMPLE_QC_DIR, 'minimum.viable.resolutions.data.tsv')
 # COVERAGE_DATA_FILE <-             file.path(COVERAGE_DIR, 'all.coverage.data.tsv')
 # hicrep results
 HICREP_DIR                     <- file.path(RESULTS_DIR, 'hicrep')
@@ -21,8 +23,9 @@ HICREP_RESULTS_FILE            <- file.path(HICREP_DIR, 'all.hicrep.scores.tsv')
 ROBINSON_REPLICATION_DIR       <- file.path(RESULTS_DIR, 'Robinson.replication')
 ROBINSON_REPLICATION_DATA_FILE <- file.path(ROBINSON_REPLICATION_DIR, 'replication.data.chr16.tsv')
 # Differential Contact results from multiHiCCompare
-SPARSE_MATRIX_DIR              <- file.path(RESULTS_DIR, 'sparse.matrices')
+# SPARSE_MATRIX_DIR              <- file.path(RESULTS_DIR, 'sparse.matrices')
 MULTIHICCOMPARE_DIR            <- file.path(RESULTS_DIR, 'multiHiCCompare')
+MULTIHICCOMPARE_RESULTS_FILE   <- file.path(MULTIHICCOMPARE_DIR, 'multiHiCCompare.results.tsv')
 # TAD Annotations
 TAD_DIR                        <- file.path(RESULTS_DIR, 'TADs')
 TAD_RESULTS_FILE               <- file.path(TAD_DIR, 'all.TAD.annotations.tsv')
