4f6aaf061492daad1dcf96e4e0c50e0ab1d9f00c
Siddharth Reed <slreed@andrew.cmu.edu>
2026-02-03 13:20:41 -0500

    generating input data with cooltools works now

--- scripts/compartments/run.compartments.cooltools.sh
+++ scripts/compartments/run.compartments.cooltools.sh
@@ -17,13 +17,12 @@ TRACK_FILES_DIR="${REFERNCE_FILES_DIR}/genome.tracks"
 # Functions
 ###################################################
 help() {
-    echo "USAGE: $(basename "${0}") [OPTIONS] {METHOD} sample1.mcool sample{2..N}.mcool
-        -m | method to use
+    echo "USAGE: $(basename "${0}") [OPTIONS] {MODE} sample1.mcool sample{2..N}.mcool
+        -m | mode: phasing, compartment
         -a | where conda is installed
-        -r | resolutions at which to annotate TADs
+        -r | resolutions at which to annotate compartments
         -o | root results dir
         -n | number of threads
-        -t | genome track file to phase and call compartments
         -h | print this message"
     exit 0
 }
@@ -35,26 +34,23 @@ get_sample_ID() {
     echo "${sample_ID}"
 }
 
-compute_gc_track_file() {
+compute_phasing_track() {
     # Make reference files to use for calling + oritenting compartments with cooltools
     local resolution="${1}"
     # bin the genome at a specific resolution
     bins_file="${GENOME_BINS_DIR}/resolution_${resolution}/genome.bins.tsv"
     if ! [[ -e "${bins_file}" ]]; then 
         mkdir -p "$(dirname "${bins_file}")"
-        cooltools genome binnify \
-            "${CHROMSIZES_FILE}" \
-            "${resolution}" >| "${bins_file}"
+        echo ${bins_file}
+        cooltools genome binnify "${CHROMSIZES_FILE}" "${resolution}" >| "${bins_file}"
     fi
     # calculate frac of nucleotides that are GC per bin
-    track_file="${TRACK_FILES_DIR}/resolution_${resolution}/genome.track.GC.tsv"
+    track_file="${TRACK_FILES_DIR}/resolution_${resolution}/${TRACK_TYPE}-genome.track.tsv"
     if ! [[ -e "${track_file}" ]]; then 
         mkdir -p "$(dirname "${track_file}")"
-        cooltools genome gc     \
-            "${bins_file}"      \
-            "${GENOME_REFERENCE}" >| "${track_file}"
+        echo "${track_file}"
+        cooltools genome ${TRACK_TYPE} ${bins_file} ${GENOME_REFERENCE} >| "${track_file}"
     fi
-    echo "${track_file}"
 }
 
 run_cooltools_compartments() {
@@ -62,28 +58,25 @@ run_cooltools_compartments() {
     local resolution="${2}"
     sample_ID="$(get_sample_ID "${sample_file}")"
     for weight_name in "${!COMPARTMENT_WEIGHTS[@]}"; do
-    for resolution in ${RESOLUTIONS[@]}; do
-        # Generate GC% track file for orienting HiC eigenvectors
-        track_file="$(compute_gc_track_file "${resolution}")"
+        for resolution in ${RESOLUTIONS[@]}; do
         # Now produce eigenvector + orient with tack file (i.e. compartment per bin)
-        param_dir="${OUTPUT_DIR}/method_${METHOD}/weight_${weight_name}/resolution_${resolution}"
+        param_dir="${OUTPUT_DIR}/method_${METHOD}/track_${TRACK_TYPE}/weight_${weight_name}/resolution_${resolution}"
         # mkdir -p "${param_dir}"
         output_prefix="${param_dir}/${sample_ID}-"
-        echo "
-        cooltools eigs-cis
-            --phasing-trace ${track_file}
-            --n-eigs 3
-            --clr-weight-name ${COMPARTMENT_WEIGHTS[${weight_name}]}
-            -o ${output_prefix}
-            ${sample_file}
-        ====================================="
-        # cooltools eigs-cis                                           \
-        #     --phasing-track "${track_file}"                          \
-        #     --n-eigs 3                                               \
-        #     --clr-weight-name ${COMPARTMENT_WEIGHTS[${weight_name}]} \
-        #     -o "${output_prefix}"                                    \
-        #     "${sample_file}"
-    done
+        # List other input files i.e. list of bins + phasing track data
+        bins_file="${GENOME_BINS_DIR}/resolution_${resolution}/genome.bins.tsv"
+        track_file="${TRACK_FILES_DIR}/resolution_${resolution}/${TRACK_TYPE}-genome.track.tsv"
+        # echo "
+        # cooltools eigs-cis
+        #     --phasing-trace ${track_file}
+        #     --n-eigs 3
+        #     --clr-weight-name ${COMPARTMENT_WEIGHTS[${weight_name}]}
+        #     -o ${output_prefix}
+        #     ${sample_file}
+        # ====================================="
+        cooltools eigs-cis --phasing-track "${track_file}" --n-eigs 3 --clr-weight-name ${COMPARTMENT_WEIGHTS[${weight_name}]} -o "${output_prefix}"                                    "${sample_file}" 
+        echo "====================================="
+        done
     done
 }
 
@@ -92,16 +85,17 @@ main() {
     local hic_samples=${*}
     source "${CONDA_DIR}/etc/profile.d/conda.sh"
     conda activate 'cooltools'
-    for resolution in ${RESOLUTIONS[@]}; do
+    if [[ ${MODE} == 'phasing' ]]; then
+        for resolution in ${RESOLUTIONS[@]}; do
+            compute_phasing_track ${resolution}
+        done
+    elif [[ ${MODE} == 'compartment' ]]; then
         for sample_file in ${hic_samples[@]}; do
             sample_ID="$(get_sample_ID "${sample_file}")"
             echo "${sample_ID}"
-            case ${METHOD} in
-                cooltools) run_cooltools_compartments "${sample_file}" "${resolution}" ;;
-                *)         echo "Invalid method: ${METHOD}" && exit 1 ;;
-            esac
+            run_cooltools_compartments "${sample_file}" ${resolution}
         done
-    done
+    fi
 }
 
 ###################################################
@@ -110,28 +104,23 @@ main() {
 # Default script arguments
 # BASE_DIR="/data/talkowski/Samples/16p_HiC"
 BASE_DIR="./"
-OUTPUT_DIR="${BASE_DIR}/results/Compartments"
+OUTPUT_DIR="${BASE_DIR}/results/compartments"
 METHOD="cooltools"
-RESOLUTIONS=(100000 50000 25000 10000)
-CONDA_DIR="$(conda info --base)"
+TRACK_TYPE="genecov"
+RESOLUTIONS=(100000 50000 25000 10000 5000)
+CONDA_DIR="${HOME}/miniforge3"
+THREADS="$(nproc)"
 # Default SLURM params
 # QUEUE="normal"; MEM_GB=30; NTASKS_PER_NODE=1; CPUS=2; THREADS=2; LOG_DIR="${BASE_DIR}/slurm.logs"
-[[ $# -eq 0 ]] && echo "No Args" && exit 1
-# while getopts "ho:l:r:n:c:q:m:a:t:e:" flag; do
+[[ $# -eq 0 ]] && echo "No Args" && exit 1 # while getopts "ho:l:r:n:c:q:m:a:t:e:" flag; do
 while getopts "hm:a:r:o:n:" flag; do
     case ${flag} in 
-        m) METHOD="${OPTARG}" ;;
+        m) MODE="${OPTARG}" ;;
+        t) TRACK_TYPE="${OPTARG}" ;;
         a) CONDA_DIR="${OPTARG}" ;;
         r) RESOLUTIONS=($(echo "${OPTARG}" | cut -d',' --output-delimiter=' ' -f1-)) ;;
         o) OUTPUT_DIR="${OPTARG}" ;;
         n) THREADS="${OPTARG}" ;;
-        # t) TRACK_FILE="${OPTARG}" ;;
-        # e) EVAL_METHOD="${OPTARG}" ;;
-        # t) THREADS="${OPTARG}" ;;
-        # m) MEM_GB="${OPTARG}" ;;
-        # c) CPUS="${OPTARG}" ;;
-        # q) QUEUE="${OPTARG}" ;;
-        # l) LOG_DIR="${OPTARG}" ;;
         h) help && exit 0 ;;
         *) echo "Invalid flag ${flag}" && help && exit 1 ;;
     esac
@@ -142,7 +131,7 @@ shift $(( OPTIND-1 ))
 # Main 
 ###################################################
 echo "
-Using Compartment caller: ${METHOD}
+Using Phasing Track:      ${TRACK_TYPE}
 Using resolution(s):      ${RESOLUTIONS[*]}
 Using output directory:   ${OUTPUT_DIR}
 Threads:                  ${THREADS}"
