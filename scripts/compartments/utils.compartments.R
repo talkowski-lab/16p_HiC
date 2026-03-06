25d0b2d71f76356e8b1bb0139dcfdd9a34171edc
Siddharth Reed <slreed@andrew.cmu.edu>
2026-03-06 12:23:11 -0500

    Initial notebook setup for compartment results

--- scripts/compartments/utils.compartments.R
+++ scripts/compartments/utils.compartments.R
@@ -307,5 +307,37 @@ run_all_HiCDOC <- function(
 }
 
 ###################################################
-# Load Compartment Results
+# Generate cooltools results
 ###################################################
+load_cooltools_compartment_results <- function(filepath){
+    filepath %>%
+    read_tsv(show_col_types=FALSE)
+}
+
+list_all_cooltools_compartment_results <- function(){
+    COMPARTMENTS_RESULTS_DIR %>%
+    parse_results_filelist(
+        suffix='-cis.vecs.tsv',
+        filename.column.name='MatrixID'
+    ) # %>% 
+    # get_info_from_MatrixIDs(keep_id=FALSE)
+}
+
+load_all_cooltools_compartment_results <- function(){
+    list_all_cooltools_compartment_results() %>%
+    mutate(
+        compartments=
+            future_pmap(
+                 .l=.,
+                 .f=load_cooltools_compartment_results,
+                 .progress=TRUE
+            )
+    ) %>%
+    dplyr::rename('chr'=chrom) %>%
+    select(-c(filepath))
+
+}
+
+post_process_cooltools_compartment_results <- function(results.df){
+}
+
